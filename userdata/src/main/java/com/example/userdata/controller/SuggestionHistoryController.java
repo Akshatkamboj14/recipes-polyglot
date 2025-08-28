42598b34b42e69de2d59d69fb96bd2479be7ce5e
Akshat <you@example.com>
2025-08-28 10:28:03 +0000

    add some files

--- userdata/src/main/java/com/example/userdata/controller/SuggestionHistoryController.java
+++ userdata/src/main/java/com/example/userdata/controller/SuggestionHistoryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/history")
+@RequestMapping("/api/history")
 public class SuggestionHistoryController {
 
     private final SuggestionHistoryService service;
